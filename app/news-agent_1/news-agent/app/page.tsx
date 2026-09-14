74dbac2821a6938d717143fccd121e0cb5992de9
mirestar01-gif <mirestar01@gmail.com>
2026-09-14 23:15:34 +0900

    정리 완료

--- app/news-agent_1/news-agent/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import Dashboard from "@/components/Dashboard";
-import { getTopics } from "@/lib/topics";
-
-export default function Home() {
-  const topics = getTopics();
-  return <Dashboard topics={topics} />;
-}
